36e25b463d31fd5ceff73a57379ddb499229c139
Sergei O. Udalov <sergei.udalov@gmail.com>
2015-12-01 20:14:00 +0300

    пользователей онлайн

--- app/models/page_view.rb
+++ app/models/page_view.rb
@@ -1,4 +1,4 @@
 class PageView < ActiveRecord::Base
-  belongs_to :session, counter_cache: true
+  belongs_to :session, counter_cache: true, touch: true
   scope :today, lambda { where("created_at > ?", Time.now.beginning_of_day ) }
 end
