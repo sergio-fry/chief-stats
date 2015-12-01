8f9e14101671ac633be6af3f3b53d0ee550a3aed
Sergei O. Udalov <sergei.udalov@gmail.com>
2015-12-02 01:08:35 +0300

    TZ later

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,4 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  around_filter :set_time_zone
-
-  def set_time_zone(&block)
-    @time_zone = current_user.try(:time_zone) || 'Moscow'
-    Time.use_zone(@time_zone, &block)
-  end
-
-  def current_user
-    # TODO
-  end
 end
