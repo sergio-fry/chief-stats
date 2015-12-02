1018dbd980fa474bfdf8d5f3ef72321f0beb6621
Sergei O. Udalov <sergei.udalov@gmail.com>
2015-12-03 02:14:47 +0300

    renew client_id cookie, session is session

--- app/controllers/collector_controller.rb
+++ app/controllers/collector_controller.rb
@@ -10,9 +10,10 @@ class CollectorController < ApplicationController
 
     # client_id - идентификатор клиента
     cookies[:client_id] ||= { value: Digest::SHA256.hexdigest("#{Time.now.to_f}#{rand}"), expires: 3.years.from_now }
+    cookies[:client_id] = { value: cookies[:client_id], expires: 3.years.from_now }
     @client = Client.find_or_create_by(cookie_id: cookies[:client_id])
 
-    cookies[:session_id] ||= { value: Digest::SHA256.hexdigest("#{Time.now.to_f}#{rand}"), expires: 30.minutes.from_now }
+    cookies[:session_id] ||= Digest::SHA256.hexdigest("#{Time.now.to_f}#{rand}")
     @user_session = @client.sessions.find_by(session_id: cookies[:session_id])
 
     @user_session ||= @client.sessions.create({
@@ -26,13 +27,6 @@ class CollectorController < ApplicationController
       url: params[:url],
       referer: params[:referer],
     })
-
-=begin
-    response.headers.delete "X-Frame-Options"
-    response.headers["Content-Security-Policy"] = "frame-ancestors *"
-    response.headers["Access-Control-Allow-Credentials"] = "true"
-    response.headers["Access-Control-Allow-Origin"] = "#{URI(request.referer).scheme}://#{URI(request.referer).host}" rescue nil
-=end
 
     render text: "OK"
   end
