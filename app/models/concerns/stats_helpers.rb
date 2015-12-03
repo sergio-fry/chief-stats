c73d0e2be08a893025570c22415c0eae11229c4b
Sergei O. Udalov <sergei.udalov@gmail.com>
2015-12-03 19:39:48 +0300

    fix!

--- app/models/concerns/stats_helpers.rb
+++ app/models/concerns/stats_helpers.rb
@@ -2,6 +2,6 @@ module StatsHelpers
   extend ActiveSupport::Concern
 
   included do
-    scope :today, lambda { |time_zone_shift=nil| where("created_at > ?", Time.now.beginning_of_day ) }
+    scope :today, lambda { |time_zone_shift=nil| where("created_at > ?", Time.zone.now.beginning_of_day ) }
   end
 end
